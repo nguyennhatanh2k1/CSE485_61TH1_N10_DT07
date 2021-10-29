8192a89c64048f20c87bbce78216828726431a6d
nguyennhatanh2k1 <nguyennhatanh2k1@gmail.com>
2021-10-29 10:00:21 +0700

    nhatanh update

---
+++ php/home.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="http://fonts.cdnfonts.com/css/kg-happy" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+
+    <title>My Blog !</title>
+</head>
+
+
+<body>
+    <div class="con">
+        <div class="nav">
+            <div class="nav-logo">
+                <a href="../php/home.php"><img src="../img/bluezone.png" alt=""></a>
+            </div>
+            <div class="nav-title">
+                <h1 style="font-family: 'KG HAPPY', sans-serif;">WELCOME HOMIES !</h1>
+            </div>
+            <div class="nav-log">
+                <a class="kg-happy-solid none-decoration" href="">SIGN IN</a> or <a class="kg-happy-solid none-decoration" href="">SIGN UP</a>
+            </div>
+        </div>
+    </div>
+    <div class="con">
+        <div class="main">
+            <img src="../img/hinh-nen-4k-tuyet-dep-cho-may-tinh.jpg" alt="">
+            <h1 class="text-center">Đây là body</h1>
+        </div>
+    </div>
+    <div class="con">
+        <div class="footer">
+            <h1>this is footer</h1>
+        </div>
+    </div>
+
+</body>
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+
+</html>
